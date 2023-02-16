b7c17c0ea5467398a1b170f82c53409d83691970
TechQuery <shiy2008@gmail.com>
2023-02-17 00:17:16 +0800

    [optimize] update Site Meta & Upstream packages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,14 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://tech-query.me/image/WebCell-0.png" />
 
         <link rel="manifest" href="/manifest.json" />
-        <script src="https://polyfill.web-cell.dev/feature/PWAManifest.js"></script>
+        <script src="https://polyfill.kaiyuanshe.cn/feature/PWAManifest.js"></script>
 
         <link
           rel="stylesheet"
-          href="https://unpkg.com/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          href="https://unpkg.com/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
         />
         <link
           rel="stylesheet"
