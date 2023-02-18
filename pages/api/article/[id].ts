78f7a0e40b6c7e30fa0ef865f9caa3237d30e0ab
TechQuery <shiy2008@gmail.com>
2023-02-18 16:33:10 +0800

    [add] Article Detail page & API

---
+++ pages/api/article/[id].ts
@@ -0,0 +1,15 @@
+import { Article } from '../../../service/Article/entity';
+import { readyDB } from '../../../service/database';
+import { safeAPI } from '../core';
+
+export default safeAPI(async ({ method, query: { id } }, response) => {
+  switch (method) {
+    case 'GET': {
+      await readyDB;
+
+      const data = await Article.findByPk(+id!);
+
+      response.send(data);
+    }
+  }
+});
--- service/database.ts
+++ service/database.ts
@@ -5,14 +5,14 @@ const NODE_ENV = process.env.NODE_ENV,
 
 export const isProduct = NODE_ENV === 'production';
 
-export const sequelize = !isProduct
+export const sequelize = isProduct
   ? new Sequelize(DATABASE_URL)
   : new Sequelize({ dialect: 'sqlite', storage: '.data/test.db' });
 
 export const readyDB = (async () => {
   await sequelize.authenticate();
 
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ alter: true });
 })();
 
 export interface ListChunk<T> {
