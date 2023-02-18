24344fee32b669414ceecc8926d570a5f2476599
TechQuery <shiy2008@gmail.com>
2023-02-18 12:04:45 +0800

    [add] Article Card & List components [add] Article model & API based on Sequelize 6

---
+++ service/database.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize';
+
+const NODE_ENV = process.env.NODE_ENV,
+  DATABASE_URL = process.env.DATABASE_URL!;
+
+export const isProduct = NODE_ENV === 'production';
+
+export const sequelize = !isProduct
+  ? new Sequelize(DATABASE_URL)
+  : new Sequelize({ dialect: 'sqlite', storage: '.data/test.db' });
+
+export const readyDB = (async () => {
+  await sequelize.authenticate();
+
+  await sequelize.sync({ force: true });
+})();
+
+export interface ListChunk<T> {
+  rows: T[];
+  count: number;
+}
